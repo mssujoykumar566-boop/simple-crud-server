1cac1db970c823846108fba214b24148c0495744
mssujoykumar566-boop <mssujoykumar566@gmail.com>
2026-05-11 15:16:36 +0600

    final-update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -34,6 +34,52 @@ const run = async () =>{
             res.send(result);
         })
 
+        app.get('/users/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+            }
+            const user = await userCollection.findOne(query);
+            
+            console.log(id,'user id')
+            res.send(user);
+        })
+
+        app.post('/users', async(req,res) => {
+            const newUser = req.body;
+            console.log(newUser,'user to be inserted')
+            const result = await userCollection.insertOne(newUser);
+            res.send(result); 
+        })
+
+        app.patch('/users/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {
+                _id: new ObjectId(id)
+            }
+            const modifiedUser = req.body;
+
+            const updatedDocument = {
+                $set: {
+                    name: modifiedUser.name,
+                    email: modifiedUser.email,
+                    role: modifiedUser.role
+                }
+            }
+
+            const result = await userCollection.updateOne(filter, updatedDocument);
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async(req,res) => {
+            const id = req.params.id;
+            const query = {
+                _id: new ObjectId(id)
+
+            }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
 
         await client.db('admin').command({ping: 1});
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
